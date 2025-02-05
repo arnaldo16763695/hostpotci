abcba5c3bf0a1155f83154fe4385bcc29004310a
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-02-05 19:20:13 -0400

    working in creater-order-payment

--- app/Views/creater-order-payment.php
+++ app/Views/creater-order-payment.php
@@ -6,34 +6,36 @@
 
 
 <div class="row align-items-center justify-content-center min-vh-100 ">
-    <div class="col col-11 col-md-5 col-lg-4 ">
+    <div class="col col-12 col-md-5 col-lg-4 ">
         <div class="card shadow-lg rounded">
             <div class="card-body p-4">
                 <h5 class="card-title text-center">Elije tu plan de internet</h5>
                 <form action="<?= base_url('createOrder') ?>" method="POST">
                     <?= csrf_field(); ?>
-                    <div class="mb-3">
+                    <div class="mb-3  p-4">
                         <div class="mb-3">
                             <label class="mb-2" for="email">Su Email:</label>
                             <input type="email" class="form-control" name="email" id="email" value="<?= set_value('email'); ?>" required autofocus>
                         </div>
-                        <div class="form-check p-2">
-                            <input class="form-check-input" type="radio" name="plan" id="1000" value="1000" checked>
-                            <label class="form-check-label" for="1000">
-                                1 hora de internet. costo: 1000 $
-                            </label>
-                        </div>
-                        <div class="form-check p-2">
-                            <input class="form-check-input" type="radio" name="plan" id="3000" value="3000">
-                            <label class="form-check-label" for="3000">
-                                2 días de internet. costo: 3000 $
-                            </label>
-                        </div>
-                        <div class="form-check p-2">
-                            <input class="form-check-input" type="radio" name="plan" id="10000" value="10000">
-                            <label class="form-check-label" for="10000">
-                                7 días de internet. costo: 7000 $
-                            </label>
+                        <div class="d-flex flex-column  ">
+                            <div class="p-2 form-check">
+                                <input class="form-check-input" type="radio" name="plan" id="1000" value="1000" checked>
+                                <label class="form-check-label" for="1000">
+                                    1 hora de internet. costo: 1.000 $
+                                </label>
+                            </div>
+                            <div class="p-2 form-check">
+                                <input class="form-check-input" type="radio" name="plan" id="3000" value="3000">
+                                <label class="form-check-label" for="3000">
+                                    2 días de internet. costo: 3.000 $
+                                </label>
+                            </div>
+                            <div class="p-2 form-check">
+                                <input class="form-check-input" type="radio" name="plan" id="10000" value="10000">
+                                <label class="form-check-label" for="10000">
+                                    7 días de internet. costo: 10.000 $
+                                </label>
+                            </div>
                         </div>
                     </div>
 
--- app/Views/hostpot.php
+++ app/Views/hostpot.php
@@ -9,28 +9,21 @@
     <div class="col col-11 col-md-5 col-lg-4 ">
         <div class="card shadow-lg rounded">
             <div class="card-body p-4">
-                <h5 class="card-title text-center">Sistema Hostpot</h5>
-                <h5 class="card-title text-center">Para continuar presione el boton Siguiente</h5>
-                <form action="<?= base_url('creater-order-payment') ?>" method="POST">
+                <h5 class="card-title text-center">Bienvenido</h5>
+                <h6 class="card-title text-center pb-4">Presione siguiente para elegir su plan de Internet</h6>
+                <form action="<?= base_url('creater-order-payment') ?>" class="border" method="POST">
                     <?= csrf_field(); ?>
-                    <!-- <div class="mb-3">
-                        <label for="name" class="form-label">Nombre:</label>
-                        <input type="text" class="form-control" id="name" name="name" value="<?= set_value('name'); ?>" placeholder="">
-                    </div>
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Rut:</label>
-                        <input type="text" class="form-control" id="rut" name="rut" value="<?= set_value('rut'); ?>" placeholder="">
-                    </div> -->
-                    <div class="mb-3">
+
+                    <div class="d-grid">
                         <button type="submit" class="btn btn-primary">Siguiente</button>
                     </div>
                     <input type="hidden" class="form-control" id="ip" name="ip" value="<?= '192.168.0.7' ?>" placeholder="">
                     <input type="hidden" class="form-control" id="mac" name="mac" value="<?= 'df:34:67:09:ab:23' ?>" placeholder="">
 
                 </form>
-               <?php if(session()->getFlashdata('errors') !== null): ?>
+                <?php if (session()->getFlashdata('errors') !== null): ?>
                     <div class="alert alert-danger my-3" role="alert">
-                         <?= session()->getFlashdata(('errors'))  ?>
+                        <?= session()->getFlashdata(('errors'))  ?>
                     </div>
 
                 <?php endif; ?>
