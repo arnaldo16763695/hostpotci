1bab303cc5da8994d1b98a69920e9feef39c53ff
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-02-10 09:51:03 -0400

    this commit fix another bug

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->post('/', 'PaymentController::creater-order-payment');
+$routes->post('/', 'PaymentController::index');
 // $routes->post('creater-order-payment', 'PaymentController::index');
 $routes->post('createOrder', 'PaymentController::createOrder');
 $routes->post('getstatuspayment', 'PaymentController::getstatuspayment');
