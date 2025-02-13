70b33207b43f3bce0ad18a8f630f1c73c8838a2c
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-02-13 19:28:24 -0400

    logo was added

--- app/Views/creater-order-payment.php
+++ app/Views/creater-order-payment.php
@@ -9,7 +9,10 @@
     <div class="col col-12 col-md-5 col-lg-4 ">
         <div class="card shadow-lg rounded">
             <div class="card-body p-4">
-                <h5 class="card-title text-center">Selecciona el tiempo de Internet que deseas.</h5>
+                <div class="pb-4">
+                    <img src="<?= base_url('/images/Logo Global-n.png') ?>" alt="Logo Globalsi" width="100" height="45">
+                </div>
+                <h5 class="card-title text-center fw-bold ">Selecciona el tiempo de Internet que deseas.</h5>
                 <form action="<?= base_url('createOrder') ?>" method="POST">
                     <?= csrf_field(); ?>
                     <div class="mb-3  p-4">
@@ -18,29 +21,29 @@
                             <input type="email" class="form-control" name="email" id="email" value="<?= set_value('email'); ?>" required autofocus>
                         </div>
                         <div class="d-flex flex-column  ">
-                           
+
                             <div class="p-2 form-check">
                                 <input class="form-check-input" type="radio" name="plan" id="3000" value="3000" checked>
                                 <label class="form-check-label" for="3000">
-                                    1 Día de Internet. $3.000 
+                                    1 Día de Internet. $3.000
                                 </label>
                             </div>
                             <div class="p-2 form-check">
                                 <input class="form-check-input" type="radio" name="plan" id="5000" value="5000">
                                 <label class="form-check-label" for="5000">
-                                    2 Días de Internet. $5.000 
+                                    2 Días de Internet. $5.000
                                 </label>
                             </div>
                             <div class="p-2 form-check">
                                 <input class="form-check-input" type="radio" name="plan" id="10000" value="10000">
                                 <label class="form-check-label" for="10000">
-                                    7 Días de Internet. $10.000 
+                                    7 Días de Internet. $10.000
                                 </label>
                             </div>
                         </div>
 
-                        <input type="hidden" class="form-control" id="ip" name="ip" value="<?= $_POST['ip']; ?>" placeholder="">
-                        <input type="hidden" class="form-control" id="mac" name="mac" value="<?= $_POST['mac']; ?>" placeholder="">
+                        <input type="hidden" class="form-control" id="ip" name="ip" value="<?= $_POST['ip'] ?>">
+                        <input type="hidden" class="form-control" id="mac" name="mac" value="<?= $_POST['mac'] ?>">
                         <div class="mb-3 mt-3 d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary">Siguiente</button>
                         </div>
