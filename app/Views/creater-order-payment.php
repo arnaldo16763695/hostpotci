37192280f30a03c348d337b5ff94080a8b50ac89
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2026-01-09 12:47:26 -0400

    change metho in form

--- app/Views/creater-order-payment.php
+++ app/Views/creater-order-payment.php
@@ -55,17 +55,12 @@
                         <!-- <input type="hidden" class="form-control" id="ip" name="ip" value=""> -->
                         <!-- <input type="hidden" class="form-control" id="mac" name="mac" value=""> -->
                         <div class="mb-3 mt-3 d-flex justify-content-between">
-                            <form action="<?= base_url(); ?>">
-                                <input type="hidden" class="form-control" id="ip" name="ip" value="<?= $_GET['ip']; ?>">
-                                <input type="hidden" class="form-control" id="mac" name="mac" value="<?= $_GET['mac']; ?>">
-                                <button type="submit" class="btn btn-danger">Atrás</button>
-                            </form>
-                            <button type="submit" class="btn btn-primary">Pagar con tarjeta</button>
+                          <button type="submit" class="btn btn-primary">Pagar con tarjeta</button>
                         </div>
 
                 </form>
                 <div class="mb-3 mt-3 d-flex justify-content-between">
-                    <form action="<?= base_url(); ?>">
+                    <form method="POST" action="<?= base_url(); ?>">
                         <input type="hidden" class="form-control" id="ip" name="ip" value="<?= $_GET['ip']; ?>">
                         <input type="hidden" class="form-control" id="mac" name="mac" value="<?= $_GET['mac']; ?>">
                         <button type="submit" class="btn btn-danger">Atrás</button>
